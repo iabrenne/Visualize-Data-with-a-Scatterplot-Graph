fc6646cbce7b0f8b807bc8a6424ebdfda2d39fcb
Irina Brennen <irinabrennen@twc.com>
2018-02-01 20:46:22 -0600

    add circles to represent data

--- js/index.js
+++ js/index.js
@@ -1,13 +1,23 @@
 const w = 1500;
 const h = 750;
 const padding = 80;
+const circleRadius = 8;
+
+minYear = d3.min(dataset, d=>d.Year);
+maxYear = d3.max(dataset, d=>d.Year);
+const xRange = maxYear - minYear;
+
+minSeconds = d3.min(dataset, d=>d.Seconds);
+maxSeconds = d3.max(dataset, d=>d.Seconds);
+const yRange = maxSeconds - minSeconds;
+
 
 const xScale =  d3.scaleLinear()
-                  .domain([d3.min(dataset, d=>d.Seconds), d3.max(dataset, d=>d.Year)])
+                  .domain([minYear - (xRange * .05), maxYear +  (xRange * .05)])
                   .range([padding, w-padding]);
 
 const yScale =  d3.scaleLinear()
-                  .domain([0,d3.max(dataset,d=>d.Seconds)])
+                  .domain([minSeconds - (yRange * .05), maxSeconds + (yRange * .05) ])
                   .range([h - padding, padding]);
 
 const svg = d3.select("body")
@@ -15,12 +25,24 @@ const svg = d3.select("body")
            .attr("width", w)
            .attr("height", h);
 
+svg.selectAll("circle")
+   .data(dataset)
+   .enter()
+   .append("circle")
+   .attr("cx", (d) => xScale(d.Year))
+   .attr("cy",(d) => yScale(d.Seconds))
+   .attr("r", (d) => circleRadius)
+   .attr("stroke","black")
+   .attr("class","dot")
+   .attr("data-xvalue", d=>d.Year)
+   .attr("data-yvalue", d=>d.Seconds)
+
 const xAxis = d3.axisBottom(xScale);
 const yAxis = d3.axisLeft(yScale);
 
 svg.append("g")
    .attr("id","x-axis")
-   .attr("transform","translate(0," + (h-padding)+")")
+   .attr("transform","translate(0," + (h-padding) + ")")
    .call(xAxis);
 
 svg.append("g")
